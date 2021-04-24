a9e14617e5f302c83182e05b38eb38dd4ff3c625
skouroufexis <skouroufexis@gmail.com>
2021-04-24 16:55:03 +0300

    created api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,69 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//GET SINGLE PRODUCT
+Route::get('/admin/products/product/{id}', function () {
+    
+    $id=request('id');
+    
+    $product=Product::find($id);
+    return ($product);
+});
+
+
+//Modify SINGLE PRODUCT
+Route::post('/admin/products/putProduct',function(){
+
+    $id=$_POST['id'];    
+    $product=Product::find($id);
+
+    if($request->hasFile('image')){
+        $imagePath = $request->file('image')->store('public/images');    
+        $product->image=$imagePath;        
+    }
+
+    $product->title=request('title');
+    $product->content=request('content');
+    $product->type=request('type');
+    $product->status=request('status');
+
+    $product->save();
+
+    return ['result'=>'Product successfully modified']; 
+
+});
+
+
+// ADD NEW PRODUCT
+Route::post('/admin/products/new',function(){
+    $product=new Product();
+
+    $id=$_POST['id'];    
+    $product=Product::find($id);
+
+    if($request->hasFile('image')){
+        $imagePath = $request->file('image')->store('public/images');    
+        $product->image=$imagePath;        
+    }
+
+    $product->title=request('title');
+    $product->content=request('content');
+    $product->type=request('type');
+    $product->status=request('status');
+
+    $product->save();
+
+return ['result'=>'New product successfully added']; 
+
+});
+
+
+// DELETE SINGLE PRODUCT    
+Route::post('/admin/products/deleteProduct',function(){    
+    $id=$_POST['id'];    
+    $product=Product::find($id);
+    $product->delete();
+    return ['result'=>$id];
 });

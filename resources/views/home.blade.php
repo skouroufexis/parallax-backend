546b7b1c2adfe872442d7aa2a5bb693493b80cef
skouroufexis <skouroufexis@gmail.com>
2021-04-24 16:52:18 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+<head>
+    
+
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+</head>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,8 +23,19 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+
+                
+
             </div>
         </div>
     </div>
+</div>
+
+<div class='container'>
+
+    <div class='row justify-centre'>
+    <a href="/admin"><button class='col col admin-button'> Go to admin page</button></a>
+    </div>
+
 </div>
 @endsection

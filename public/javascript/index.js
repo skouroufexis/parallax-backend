6cb0ae178c1eb7e3874baec8671807e080f262b7
skouroufexis <skouroufexis@gmail.com>
2021-04-24 16:54:07 +0300

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,15 +1,4 @@
 
-let productContainers=document.getElementsByClassName('product-container');
-let l=productContainers.length;
-let c;
-
-for (c=0;c<l;c++){
-    if(c!=0){
-        let path='./images/products'+c+'.png';
-        productContainers[c].style.backgroundImage='url('+path+')';
-
-    }
-}
 
 function openMenu(){
 
